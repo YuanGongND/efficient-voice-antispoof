c334e9e86ced1683a9d75817f8c81776e2b5e674
Jerry Yang <jyang9@nd.edu>
2020-10-15 11:25:17 -0400

    remove uncessary import

--- model_AFN/src/v4_plot.py
+++ model_AFN/src/v4_plot.py
@@ -1,12 +1,10 @@
 from __future__ import print_function
-import os 
+import os
 import torch
 import numpy as np
 import logging
 import torch.nn.functional as F
-from v1_metrics import compute_eer
-import data_reader.kaldi_io as ko
-import data_reader.adv_kaldi_io as ako
+from .v1_metrics import compute_eer
 import matplotlib
 matplotlib.use('Agg')
 from matplotlib import pyplot as plt
@@ -14,7 +12,8 @@ from matplotlib import pyplot as plt
 ## Get the same logger from main"
 logger = logging.getLogger("anti-spoofing")
 
-def retrieve_weight(args, model, device, eval_loader, eval_scp, eval_utt2label, plot_wd, rnn=False):
+
+def retrieve_weight(args, model, device, eval_loader, plot_wd, rnn=False):
     """get the attention weight, batch size has to be 1
     """
     logger.info("Starting evaluation")
@@ -25,24 +24,23 @@ def retrieve_weight(args, model, device, eval_loader, eval_scp, eval_utt2label, 
         for id_list, data, target in eval_loader:
             assert len(id_list) == 1, 'batch size has to set to 1'
             data, target = data.to(device), target.to(device)
-            target = target.view(-1,1).float()
+            target = target.view(-1, 1).float()
             #_, weight = model(data) 
             #mat = weight # heatmap of the weight 
             #mat = data + data * weight 
             mat = data
             mat = mat.data.cpu().numpy()
-            make_plot(id_list[0], mat, plot_wd) 
+            make_plot(id_list[0], mat, plot_wd)
+
 
 def make_plot(utt_id, mat, plot_wd):
-    """plot logspec with attention 
+    """plot logspec with attention
     """
-    mat = mat.reshape(257,1091)
+    mat = mat.reshape(257, 1091)
     #mat = mat.T
     #mat = mat.reshape(257,160)
-    
+
     fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(20,4))
     cax = ax.matshow(mat, interpolation='nearest', aspect='auto', cmap=plt.cm.YlOrBr, vmin=0.0, vmax=1.0, origin='lower')
     fig.colorbar(cax)
     plt.savefig(plot_wd + utt_id + '.pdf', bbox_inches='tight')
-
-
--- model_AFN/src/v4_prediction.py
+++ model_AFN/src/v4_prediction.py
@@ -1,23 +1,21 @@
-import torch
-import numpy as np
 import logging
-import torch.nn.functional as F
 from .v1_metrics import compute_eer
 # import data_reader.adv_kaldi_io as ako
 from .v4_validation import compute_loss, utt_scores
 
 """
-correspond to v3_validation, threshold-less EER 
+correspond to v3_validation, threshold-less EER
 """
 
-## Get the same logger from main"
+# Get the same logger from main"
 logger = logging.getLogger("anti-spoofing")
+
 
 def prediction(args, model, device, eval_loader, eval_utt2label, rnn=False):
     logger.info("Starting evaluation")
     eval_loss, eval_scores = compute_loss(model, device, eval_loader, rnn)
     eval_preds, eval_labels = utt_scores(eval_scores, eval_utt2label)
-    eval_eer  = compute_eer(eval_labels, eval_preds)
+    eval_eer = compute_eer(eval_labels, eval_preds)
 
     logger.info("===> Final predictions done. Here is a snippet")
     logger.info('===> evalidation set: Average loss: {:.4f}\tEER: {:.4f}\n'.format(
@@ -25,12 +23,12 @@ def prediction(args, model, device, eval_loader, eval_utt2label, rnn=False):
 
     return eval_loss, eval_eer
 
-def scores(args, model, device, eval_loader, eval_scp, eval_utt2label, rnn=False):
+
+def scores(args, model, device, eval_loader, eval_utt2label, rnn=False):
     """get the scores only (for averaging across models later on)
     """
-    #logger.info("Starting evaluation")
+    # logger.info("Starting evaluation")
     eval_loss, eval_scores = compute_loss(model, device, eval_loader, rnn)
-    eval_preds, eval_labels = utt_scores(eval_scores, eval_scp, eval_utt2label)
+    eval_preds, eval_labels = utt_scores(eval_scores, eval_utt2label)
 
     return eval_preds, eval_labels
-
